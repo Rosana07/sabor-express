87668704b7ac200ab05d448773cfd23190fa0a7e
Rosana07 <emaildonotreply>
2024-07-10 21:51:34 -0300

    novas funções

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
 
+restaurantes = ['LeSushi', 'Kony']
 
 def exibir_nome_do_programa():
 
     print("""
       
-      ssǝɹdxǝ ɹoqɐS
+        
+░█▀▀▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀█ 　 ░█▀▀▀ █─█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ 
+─▀▀▀▄▄ █▄▄█ █▀▀▄ █──█ █▄▄▀ 　 ░█▀▀▀ ▄▀▄ █──█ █▄▄▀ █▀▀ ▀▀█ ▀▀█ 
+░█▄▄▄█ ▀──▀ ▀▀▀─ ▀▀▀▀ ▀─▀▀ 　 ░█▄▄▄ ▀─▀ █▀▀▀ ▀─▀▀ ▀▀▀ ▀▀▀ ▀▀▀
       
       """)
 
@@ -17,21 +21,47 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 
- 
+def opcao_invalida():
+    print('Opção invalida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
+def listar_restaurantes():
+    os.system('cls')
+    print('Listando os restaurantes\n')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
 def escolher_opcao():
     opcao_escolhida = int(input('\nEscolha uma opção: '))
     # opcao_escolhida = int(opcao_escolhida)
+    try:
 
-
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurantes')
-    else:
-        finalizar_app()
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar restaurantes')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 
 def finalizar_app():
@@ -41,6 +71,7 @@ def finalizar_app():
 
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
